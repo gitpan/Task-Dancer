233ea7d7e058a35acb61a685bea9c2c071b8b32f
Alberto Manuel Brandão Simões <ambs@perl-hackers.net>
2011-11-05 22:05:59 +0000

    Import of AMBS/Task-Dancer-0.34 from CPAN.
    gitpan-cpan-distribution: Task-Dancer
    gitpan-cpan-version:      0.34
    gitpan-cpan-path:         AMBS/Task/Task-Dancer-0.34.tar.gz
    gitpan-cpan-author:       AMBS
    gitpan-cpan-maturity:     released

--- lib/Task/Dancer.pm
+++ lib/Task/Dancer.pm
@@ -1,6 +1,6 @@
 package Task::Dancer;
 
-our $VERSION = '0.33';
+our $VERSION = '0.34';
 
 1;
 
@@ -12,7 +12,7 @@ Task::Dancer - Dancer in a box
 
 =head1 VERSION
 
-Version 0.33
+Version 0.34
 
 =head1 DESCRIPTION
 
@@ -46,7 +46,7 @@ if any module temporarily disabled is working again.
 
 =item L<Dancer::Template::MojoTemplate>
 
-=item L<Dancer::Template::Semantic>
+=item L<Dancer::Template::Semantic> (disabled)
 
 =item L<Dancer::Template::TemplateFlute>
 
@@ -92,6 +92,8 @@ if any module temporarily disabled is working again.
 
 =over 4
 
+=item L<Dancer::Session::CHI> # disabled
+
 =item L<Dancer::Session::Cookie>
 
 =item L<Dancer::Session::KiokuDB>
@@ -134,6 +136,8 @@ if any module temporarily disabled is working again.
 
 =item L<Dancer::Plugin::DirectoryView>
 
+=item L<Dancer::Plugin::Dispatcher>
+
 =item L<Dancer::Plugin::ElasticSearch>
 
 =item L<Dancer::Plugin::Email>
@@ -156,6 +160,8 @@ if any module temporarily disabled is working again.
 
 =item L<Dancer::Plugin::FormValidator>
 
+=item L<Dancer::Plugin::Hosts>
+
 =item L<Dancer::Plugin::LibraryThing>
 
 =item L<Dancer::Plugin::Memcached>
@@ -166,6 +172,8 @@ if any module temporarily disabled is working again.
 
 =item L<Dancer::Plugin::Mongo>
 
+=item L<Dancer::Plugin::Mongoose>
+
 =item L<Dancer::Plugin::MPD>
 
 =item L<Dancer::Plugin::NYTProf>
@@ -173,6 +181,10 @@ if any module temporarily disabled is working again.
 =item L<Dancer::Plugin::ORMesque>
 
 =item L<Dancer::Plugin::Params::Normalization>
+
+=item L<Dancer::Plugin::Passphrase>
+
+=item L<Dancer::Plugin::Preprocess::Sass>
 
 =item L<Dancer::Plugin::Progress>
 
@@ -190,13 +202,15 @@ if any module temporarily disabled is working again.
 
 =item L<Dancer::Plugin::SMS>
 
-=item L<Dancer::Plugin::SporeDefinitionControl>
+=item L<Dancer::Plugin::SporeDefinitionControl> (disabled)
 
 =item L<Dancer::Plugin::Stomp>
 
 =item L<Dancer::Plugin::TimeRequest>
 
 =item L<Dancer::Plugin::Thumbnail>
+
+=item L<Dancer::Plugin::ValidateTiny>
 
 =item L<Dancer::Plugin::ValidationClass>
 
